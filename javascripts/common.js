8d9ee8688df3557899366ae42a287ebf44558ef3
AndreyKondakov <primax7k@gmail.com>
2018-02-12 12:57:23 +0200

    add button pause

--- javascripts/common.js
+++ javascripts/common.js
@@ -10,3 +10,26 @@ function nextSlide() {
   slides[currentSlide].className = 'slide showing';
 }
 
+// button pause
+var playing = true;
+var pauseButton = document.getElementById('pause');
+
+function pauseSlideshow() {
+  pauseButton.innerHTML = 'Play';
+  playing = false;
+  clearInterval(slideInterval);
+}
+
+function playSlideshow() {
+  pauseButton.innerHTML = 'Pause';
+  playing = true;
+  slideInterval = setInterval(nextSlide,2000);
+}
+
+pauseButton.onclick = function() {
+  if(playing) {
+    pauseSlideshow();
+  } else {
+    playSlideshow();
+  }
+};
